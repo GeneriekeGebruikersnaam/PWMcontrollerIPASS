5596e3b3994e8b43241204806d8982636981d8ef
GeneriekeGebruikersnaam <jjoram00@gmail.com>
2019-07-11 10:00:11 +0200

    Small start regarding implementation

--- codeMain/PWMcontroller.cpp
+++ codeMain/PWMcontroller.cpp
@@ -1,9 +1,32 @@
 #include "PWMcontroller.hpp"
 
-void PWMcontroller::sendChannel(const int &channel)
+void PWMcontroller::writeChannel(const int &channel, int[6] message)
 {
     if(channel <= PWMcontroller::channels && channel > 0)
     {
-        // send a command to a channel
+        int[8] toWrite;
+        toWrite[0] = 1;
+        toWrite[7] = 0;
+        for(int i = 1; i < 7)
+        {
+            toWrite[i] = message[i-1];
+        }
     }
+
+    // Write int[8] toWrite to PWM controller
+
+}
+
+uint8_t PWMcontroller::readChannel(const int &channel)
+{
+    if(channel <= PWMcontroller::channels && channel > 0)
+    {
+
+    }
+}
+
+void PCA9685::setFrequency(int frequency)
+{
+    (PWMcontroller::oscillatorClock / (4096 * frequency)) - 1;
+    // send to PRE_SCALE
 }
--- codeMain/PWMcontroller.hpp
+++ codeMain/PWMcontroller.hpp
@@ -2,6 +2,7 @@
 #define PWMCONTROLLER_HPP
 
 #include "hwlib.hpp"
+#include <stdint.h>
 
 
 /// @file
@@ -17,18 +18,32 @@ class PWMcontroller
     private:
         const int bits;
         const int channels;
-        const bool oscillator;
+        const bool oscillator = false;
+    protected:
+        const int oscillatorClock = 0;
     public:
 
 /// \brief
-/// Constructor for a PWM controller
+/// Constructor for a PWM controller without an oscillator
 /// \details
-/// This constructor asks for an oscillator, though no value is needed as it'll automatically assume one is present.
-/// If a controller without an oscillator were to be used, the value can simply be set to false in the constructor.
-        PWMcontroller(int bits = 0, int channels = 0, bool oscillator = true):
+/// This is a constructor for a PWM controller without an oscillator built in.
+/// It just sets the number of bits and channels the chip has.
+        PWMcontroller(int bits = 0, int channels = 0):
+            bits(bits),
+            channels(channels),
+            {}
+
+/// \brief
+/// Constructor for a PWM controller with an oscillator
+/// \details
+/// This is a constructor for a PWM controller with an oscillator built in.
+/// It sets the bits, channels, whether an oscillator is present or not, and the oscillator's clock frequency in Hz.
+/// The program assumes the oscillator's clock frequency is a constant value.        
+        PWMcontroller(int bits, int channels, bool oscillator, int oscillatorClock):
             bits(bits),
             channels(channels),
             oscillator(oscillator)
+            oscillatorClock(oscillatorClock)
             {}
 
 /// \brief
@@ -37,8 +52,20 @@ class PWMcontroller
 /// This function sends a given signal to a given channel.
 /// It is checked if the given channel is within the range of channels corresponding to the chip.
 /// If the channel isn't within that range, it'll do nothing and skip the function entirely.
-        void sendChannel(const int &channel); 
+        void writeChannel(const int &channel, int[6] message);
 
+/// \brief
+/// Reads a channel
+/// \details
+/// This function is used to read what is currently stored in any given register.
+        uint8_t readChannel(const int &channel);
+
+/// \brief
+/// Sets a PWM controller's frequency
+/// \details
+/// This function sets a PWM controller's internal frequency.
+/// This function is not worked out as I have no clue if there's a universal formula for frequencies.
+        void setFrequency(int frequency);
 };
 
 /// \brief
@@ -59,8 +86,15 @@ class PCA9685 : public PWMcontroller
 /// The PCA9685 has 16 channels, which is set in the constructor.
 /// The PCA9685 has 12 bits and this is set in the constructor.
         PCA9685():
-            PWMcontroller(12, 16)     // 12 bits and 16 channels are the PCA9685's values.
-            {}
+            PWMcontroller(12, 16, true, 25'000'000)     // 12 bits and 16 channels are the PCA9685's values.
+            {}                                          // There is an oscillator and its clock is 25MHz.
+
+/// \brief
+/// Sets PCA9685's frequency
+/// \details
+/// This function sets the PCA9685's onboard frequency.
+/// This function is PCA9685 specific and is overridden.
+        override void setFrequency(int frequency);
         
 };
 
